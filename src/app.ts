4d56d085509d1748eee35549f19b9953de589054
MinThantMaung <minthant180@gmail.com>
2025-04-27 01:29:00 +0900

    first commit

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import helmet from "helmet";
+import compression from "compression";
+import cors from "cors";
+import morgan from "morgan";
+
+export const app = express();
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
