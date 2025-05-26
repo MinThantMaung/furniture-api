c685223330f2902acd90732a3ac7bc1b353cf098
MinThantMaung <minthant180@gmail.com>
2025-05-26 17:56:29 +0900

    feat:update code

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,12 +1,27 @@
 import { NextFunction, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { min } from "moment";
 
-export const register = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  res.status(200).json({ message: "register" });
-};
+export const register = [
+  body("phone", "Invalid phone number")
+    .trim()
+    .notEmpty()
+    .withMessage("Phone number cannot be empty.")
+    .matches("^[0-9]+$")
+    .withMessage("Please enter only number.")
+    .isLength({ min: 5, max: 12 })
+    .withMessage("Phone number is between 2 and 12 digits."),
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req).array({ onlyFirstError: true });
+    if (errors.length > 0) {
+      const error: any = new Error(errors[0].msg);
+      error.status = 400;
+      error.code = "Error_Invalid";
+      return next(error);
+    }
+    res.status(200).json({ message: "register success" });
+  },
+];
 
 export const varifyOtp = async (
   req: Request,
